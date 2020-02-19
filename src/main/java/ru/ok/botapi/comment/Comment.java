e57e0f60a2664ce9a2670ba2e29f83c8f46f7b8d
Andrey <andreystar2403@gmail.com>
2020-02-19 12:07:30 +0300

    Modify Comment class

--- src/main/java/ru/ok/botapi/comment/Comment.java
+++ src/main/java/ru/ok/botapi/comment/Comment.java
@@ -1,5 +1,38 @@
 package ru.ok.botapi.comment;
 
 public class Comment {
+    
     private long postId;
+    private long commentId;
+    private String text;
+    
+    public Comment(long postId, long commentId, String text) {
+        this.postId = postId;
+        this.commentId = commentId;
+        this.text = text;
+    }
+    
+    public long getPostId() {
+        return postId;
+    }
+    
+    public void setPostId(long postId) {
+        this.postId = postId;
+    }
+    
+    public long getCommentId() {
+        return commentId;
+    }
+    
+    public void setCommentId(long commentId) {
+        this.commentId = commentId;
+    }
+    
+    public String getText() {
+        return text;
+    }
+    
+    public void setText(String text) {
+        this.text = text;
+    }
 }
