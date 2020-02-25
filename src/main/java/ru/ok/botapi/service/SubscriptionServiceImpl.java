c241a49fc5eb3d77568c421d7c69321884fd6de6
Denis <denis.rezni@gmail.com>
2020-02-25 17:08:13 +0300

    Added findByPostId

--- src/main/java/ru/ok/botapi/service/SubscriptionServiceImpl.java
+++ src/main/java/ru/ok/botapi/service/SubscriptionServiceImpl.java
@@ -31,9 +31,14 @@ public class SubscriptionServiceImpl implements SubscriptionService {
 		if (result.isPresent()) {
 			return result.get();
 		}
-		throw new RuntimeException("Did not find post id - " + id);
+		throw new RuntimeException("Did not find subscription id - " + id);
 	}
-	
+
+	@Override
+	public List<Subscription> findByPostId(long postId) {
+		return subscriptionRepository.findByPostId(postId);
+	}
+
 	@Override
 	public void save(Subscription subscription) {
 		subscriptionRepository.save(subscription);
